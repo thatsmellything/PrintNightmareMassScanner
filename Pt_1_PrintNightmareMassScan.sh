928df6a28d55b00fcf8511b1412cfdb8e7e2343b
thatsmellything <thesmelliestman@gmail.com>
2021-09-30 09:31:14 -0600

    Fixed setup

--- Pt_1_PrintNightmareMassScan.sh
+++ Pt_1_PrintNightmareMassScan.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo -n "Go through setup?(y/n): "
-read VAR
-if [[$VAR = "y"]]
-then
+
+echo "Go through setup?(y/n): "
+read uservar
+#read -p 'Go through setup?(y/n): ' uservar
+
+if [[ "$uservar" == "y" ]]; then
+	rm -rf ItWasAllADream/
 	echo "Gonna run through setup"
 	git clone https://github.com/byt3bl33d3r/ItWasAllADream
 	sudo apt update
 	sudo apt install python3-pip
 	curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
-	pip install --user poetry
+	pip install --user poetry;
 
 else
 	cd ItWasAllADream && poetry install && poetry shell
